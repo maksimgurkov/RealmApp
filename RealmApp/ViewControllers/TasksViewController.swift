6159a2e4415ed760d8897486749a99fd63a24f46
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-04-07 19:23:10 +0300

    Initial Commit

---
+++ RealmApp/ViewControllers/TasksViewController.swift
@@ -0,0 +1,127 @@
+//
+//  TasksViewController.swift
+//  RealmApp
+//
+//  Created by Alexey Efimov on 02.07.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class TasksViewController: UITableViewController {
+    
+    var taskList: TaskList!
+    
+    private var currentTasks: Results<Task>!
+    private var completedTasks: Results<Task>!
+    
+    private let storageManager = StorageManager.shared
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = taskList.title
+        
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonPressed)
+        )
+        navigationItem.rightBarButtonItems = [addButton, editButtonItem]
+        currentTasks = taskList.tasks.filter("isComplete = false")
+        completedTasks = taskList.tasks.filter("isComplete = true")
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        section == 0 ? currentTasks.count : completedTasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        section == 0 ? "CURRENT TASKS" : "COMPLETED TASKS"
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TasksCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let task = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
+        content.text = task.title
+        content.secondaryText = task.note
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let task = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
+        
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { [unowned self] _, _, _ in
+            storageManager.delete(task)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        let edit = UIContextualAction(style: .normal, title: "Edit") { [unowned self] _, _, isDone in
+            showAlert(with: task) {
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+            isDone(true)
+        }
+        
+        let title = indexPath.section == 0 ? "Done" : "Undone"
+        
+        let done = UIContextualAction(style: .normal, title: title) { [unowned self] _, _, isDone in
+            storageManager.done(task)
+            
+            let indexCurrentTask = IndexPath(row: self.currentTasks.count - 1, section: 0)
+            let indexCompletedTask = IndexPath(row: self.completedTasks.count - 1, section: 1)
+            let index = indexPath.section == 0 ? indexCompletedTask : indexCurrentTask
+
+            tableView.moveRow(at: indexPath, to: index)
+            isDone(true)
+        }
+        
+        edit.backgroundColor = .orange
+        done.backgroundColor = .green
+        
+        
+        
+        
+        return UISwipeActionsConfiguration(actions: [done, edit, delete])
+    }
+    
+    @objc private func addButtonPressed() {
+        showAlert()
+    }
+
+}
+
+extension TasksViewController {
+    private func showAlert(with task: Task? = nil, completion: (() -> Void)? = nil) {
+        let taskAlertFactory = TaskAlertControllerFactory(
+            userAction: task != nil ? .editTask : .newTask,
+            taskTitle: task?.title,
+            taskNote: task?.note
+        )
+        let alert = taskAlertFactory.createAlert { [weak self] taskTitle, taskNote in
+            if let task, let completion {
+                self?.storageManager.edit(task, taskNote: task, newTitle: taskTitle, newNote: taskNote)
+                completion()
+            } else {
+                self?.save(task: taskTitle, withNote: taskNote)
+            }
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    private func save(task: String, withNote note: String) {
+        storageManager.save(task, withTaskNote: note, to: taskList) { task in
+            let rowIndex = IndexPath(row: currentTasks.index(of: task) ?? 0, section: 0)
+            tableView.insertRows(at: [rowIndex], with: .automatic)
+        }
+    }
+}
